446ba6ce50c94b388fa3f79e31b3e3de1a9fbce7
matthiasoo <makky.x.55@gmail.com>
2026-03-15 22:01:05 +0100

    State on higher level

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import './App.css'
 import InventoryCard from './components/InventoryCard.jsx'
 
 function App() {
+    const [inventory, setInventory] = useState([
+        { id: 1, name: "Hammer", quantity: 20 },
+        { id: 2, name: "Screwdriver", quantity: 20 },
+        { id: 3, name: "Shovel", quantity: 20 },
+        { id: 4, name: "Axe", quantity: 20 },
+    ]);
+
+    const updateQuantity = function(id, change) {
+        const newArray = inventory.map(item => {
+            if (item.id === id) return { ...item, quantity: change }
+            else return item;
+        });
+        
+        setInventory(newArray);
+    };
 
     return (
         <>
             <h1>Smart Workshop Management System</h1>
+            <h2>Total Number of items: {inventory.reduce((ac, cur) => ac + cur.quantity, 0)}</h2>
             <section className='products-list'>
-                <InventoryCard name={"Hammer"} quantity={20} />
-                <InventoryCard name={"Screwdriver"} quantity={20} />
-                <InventoryCard name={"Shovel"} quantity={20} />
-                <InventoryCard name={"Axe"} quantity={20} />
+                {inventory.map(item => <InventoryCard key={item.id} item={item} update={updateQuantity} />)}
             </section>
         </>
     )
--- src/components/InventoryCard.jsx
+++ src/components/InventoryCard.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
 import './InventoryCard.css'
 
-function InventoryCard({ name, quantity }) {
-    const [number, setNumber] = useState(quantity);
-    const isOutOfStock = number === 0;
+function InventoryCard({ item, update }) {
+    const isOutOfStock = item.quantity === 0;
 
     return (
         <div className={`card ${isOutOfStock ? 'card-out' : ''}`}>
-            <h1 className='name'>{name}</h1>
+            <h1 className='name'>{item.name}</h1>
             <div className='quantity-panel'>
                 <button className={`control ${isOutOfStock ? 'control-out' : ''}`} onClick={() => {
-                    if (number > 0) setNumber(number - 1)
+                    if (item.quantity > 0) update(item.id, item.quantity - 1);
                 }}>-</button>
-                <span>{number}</span>
-                <button className={`control ${isOutOfStock ? 'control-out' : ''}`} onClick={() => setNumber(number + 1)}>+</button>
+                <span>{item.quantity}</span>
+                <button className={`control ${isOutOfStock ? 'control-out' : ''}`} 
+                    onClick={() => update(item.id, item.quantity + 1)}>+</button>
             </div>
             {isOutOfStock && <h4 className='out-of-stock-msg'>Out of Stock!</h4>}
         </div>
